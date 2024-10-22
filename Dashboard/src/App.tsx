264027a545ddbcf3329b505cf67302313f1bc193
Arun <arunkumar.zeoner@gmail.com>
2024-10-22 15:54:53 +0530

    build files

--- Dashboard/src/App.tsx
+++ Dashboard/src/App.tsx
@@ -1,50 +1,16 @@
+import './App.css'
 
-import React, { useEffect, useState } from 'react';
-import FHIR from 'fhirclient';
-
-const App = () => {
-  const [patient, setPatient] = useState<any>(null);
-
-  useEffect(() => {
-    FHIR.oauth2.ready().then((client: any) => {
-      client.request(`Patient/${client.patient.id}`).then((pt: any) => {
-        setPatient(pt);
-      });
-    }).catch(console.error);
-  }, []);
+function App() {
 
   return (
-    <div>
-      <h1>FHIR Patient Data</h1>
-      {patient ? (
-        <div>
-          <p>First Name: {patient.name[0].given[0]}</p>
-          <p>Last Name: {patient.name[0].family}</p>
-          <p>Gender: {patient.gender}</p>
-          <p>Date of Birth: {patient.birthDate}</p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
-  );
-};
+    <>
+     <div>
+     <h1 className="text-3xl  font-bold underline">
+      Hello world!
+    </h1>
+     </div>
+    </>
+  )
+}
 
-export default App;
-
-// import './App.css'
-
-// function App() {
-
-//   return (
-//     <>
-//      <div>
-//      <h1 className="text-3xl  font-bold underline">
-//       Hello world!
-//     </h1>
-//      </div>
-//     </>
-//   )
-// }
-
-// export default App
+export default App
