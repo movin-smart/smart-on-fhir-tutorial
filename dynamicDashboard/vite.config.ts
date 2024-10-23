849577ca341977b4f4f3d74672ee83a398c8348e
Arun <arunkumar.zeoner@gmail.com>
2024-10-23 12:47:28 +0530

    dashboard3

--- dynamicDashboard/vite.config.ts
+++ dynamicDashboard/vite.config.ts
@@ -1,12 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//       	  base: 'OpenEMR/interface/modules/custom_modules/dynamicDashboard/dist/', // Set the base URL for asset loading
+// 	plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-      	  base: 'OpenEMR/interface/modules/custom_modules/dynamicDashboard/dist/', // Set the base URL for asset loading
-	plugins: [react()],
+  plugins: [react()],
 })
-
-
 
 
